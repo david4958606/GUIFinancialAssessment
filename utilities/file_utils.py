9773456de8ab868970e4ba481fd8d6105d133ed9
David Wang <david.wang@kirinou.top>
2024-09-03 21:38:17 +0800

    refactor(file_utils): add methods for managing result.txt file

--- utilities/file_utils.py
+++ utilities/file_utils.py
@@ -7,3 +7,15 @@ class FileUtils:
         # Check if the "result.txt" file exists in the root directory
         if not os.path.exists("result.txt"):
             return False
+        
+    def new_result_file():
+        # Create a new empty "result.txt" file if not already present
+        # Or clean the existing file
+        with open("result.txt", "w") as file:
+            file.write("")
+
+    def amend_result_file(data):
+        # Append data to the "result.txt" file
+        with open("result.txt", "a") as file:
+            file.write(data)
+            file.write("\n")
