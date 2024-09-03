dbb32d1b909c992b0ecec129f5430d898b2bf44c
David Wang <david.wang@kirinou.top>
2024-09-03 21:07:21 +0800

    refactor(main_ui_controller): update button click event handler

--- controllers/main_ui_controller.py
+++ controllers/main_ui_controller.py
@@ -1,7 +1,8 @@
-import os
 from PyQt6 import QtWidgets
 from views.main_ui import Ui_Main  # 导入生成的UI类
 from controllers.fin_obj_ui_controller import FinancialObjectiveController
+from utilities.ui_utils import UiUtils  # Importing the UiUtils class
+
 
 class MainUiController:
     def __init__(self):
@@ -12,7 +13,7 @@ class MainUiController:
         # 连接按钮点击事件到槽函数
         self.fin_obj_controller = FinancialObjectiveController()
         self.ui.pushButton.clicked.connect(self.open_financial_objective)
-        self.ui.pushButton_2.clicked.connect(self.check_result_file)
+        self.ui.pushButton_2.clicked.connect(UiUtils.show_file_not_found_warning)
 
     def show_main_window(self):
         self.main_window.show()
@@ -20,17 +21,3 @@ class MainUiController:
     def open_financial_objective(self):
         print("MainUIController: open_financial_objective called")  # Debug print
         self.fin_obj_controller.show()
-
-    def check_result_file(self):
-        # 检查根目录下是否存在 "result.txt" 文件
-        if not os.path.exists("result.txt"):
-            self.show_warning("No Result Find")
-
-    def show_warning(self, message):
-        # 弹出警告框
-        msg_box = QtWidgets.QMessageBox()
-        msg_box.setIcon(QtWidgets.QMessageBox.Icon.Warning)
-        msg_box.setWindowTitle("Warning")
-        msg_box.setText(message)
-        msg_box.setStandardButtons(QtWidgets.QMessageBox.StandardButton.Ok)
-        msg_box.exec()
