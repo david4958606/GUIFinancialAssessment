6d17fc1201455f1a705af2b168425df6d72d22e9
David Wang <david.wang@kirinou.top>
2024-09-04 20:51:56 +0800

    feat: Add test for finding string in file

---
+++ test/test_utilities.py
@@ -0,0 +1,6 @@
+from utilities.file_utils import FileUtils
+
+test_text = """Question: 1. I have a diploma or higher qualification in finance, business or accounting\nAnswer: Yes"""
+
+if FileUtils.find_string_in_file(test_text):
+    print("String found in file")
